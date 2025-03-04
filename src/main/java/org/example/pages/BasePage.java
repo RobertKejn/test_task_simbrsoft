07611c5e21c1d54ba9fc1e9a3217980ccaaedc8e
Raymond <robertkejn@gmail,com>
2025-03-05 00:45:31 +0400

    Selenium Sesting (cleared imports)

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -4,11 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
 
--- src/main/java/org/example/pages/FormFieldsPage.java
+++ src/main/java/org/example/pages/FormFieldsPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.text.Element;
-import javax.swing.text.html.Option;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
